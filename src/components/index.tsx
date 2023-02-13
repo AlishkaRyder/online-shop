4ab16c9de9a2bf6c6a936f3c2d1dbea1dd6d8f55
AlishkaRyder <AlishkaRyder@gmail.com>
2023-02-13 21:41:28 +0600

    some commit

---
+++ src/components/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+interface HeaderProps {
+    logo:string
+}
+
+const Header: React.FC<HeaderProps> = ({ logo }) => {
+  return <div>Header</div>
+}
+
+export default Header
